70b74fd97624943d1b78ec0cd6999fec7aa36af2
Khudaat E. Mammadli <khudaatem@code.local>
2024-10-19 15:04:57 +0400

    add folder

---
+++ n ededi/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tam eded daxil edin");
+            int number=Convert.ToInt32(Console.ReadLine());
+
+            if(number<0)
+            {
+                number= -number;
+            }
+            if (number == 0)
+            {
+                Console.WriteLine("Ededin mertebe sayi:1"); 
+                return;
+            }
+            int countnumber = 0;
+            while (number > 0)
+            {
+                number/= 10;
+                countnumber++;
+            }
+
+            Console.WriteLine("Ededin mertebe sayi; " + countnumber);
+        }
+    }
+}
